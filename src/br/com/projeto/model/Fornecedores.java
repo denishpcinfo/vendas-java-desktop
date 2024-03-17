823660125c7d0252e4e21c64e75e3128dab0d504
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2024-03-17 16:12:16 -0300

    java-dev

---
+++ src/br/com/projeto/model/Fornecedores.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.model;
+
+public class Fornecedores extends  Clientes {
+    
+    //atributos
+    private String cnpj;    
+    
+    //getters e setters
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    @Override
+    public String toString(){
+        return this.getNome();        
+    }
+    
+    
+}
